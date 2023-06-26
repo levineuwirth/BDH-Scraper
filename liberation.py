7f368af4c77d6908ce7254eda9c624aa02206af9
Levi Neuwirth <63686838+levineuwirth@users.noreply.github.com>
2023-06-26 15:37:10 -0400

    init

---
+++ liberation.py
@@ -0,0 +1,31 @@
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+def main():
+    driver = webdriver.Chrome('/Users/levineuwirth/Downloads/chrome-mac-x64/Google Chrome for Testing.app')
+    driver.get('https://www.browndailyherald.com/')
+
+    # Wait for the page to load (adjust the delay as needed)
+    time.sleep(2)  # 2 seconds delay
+
+    page_source = driver.page_source
+
+    # Get the links which are to other BDH pages.
+    # We initially got the main page, so we need to find the latest articles.
+    soup = BeautifulSoup(page_source, 'html.parser')
+    links = soup.find_all('a')
+    bdh_links = []
+
+    # Filter links that start with 'https://www.browndailyherald.com/'
+    for link in links:
+        href = link.get('href')
+        if href and href.startswith('https://www.browndailyherald.com/'):
+            bdh_links.append(href)
+
+    print(bdh_links)
+
+    driver.quit()
+
+if __name__ == '__main__':
+    main()
